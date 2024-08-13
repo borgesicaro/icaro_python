3ba045b467fc654a748a88f2dd002b9a0e7d0461
borgesicaro <icaroribeiro2004@gmail.com>
2024-08-13 20:52:18 -0300

    primeira atividade 13/8/2024

---
+++ aula_001.py
@@ -0,0 +1,8 @@
+nome=input("Insira seu nome")
+matricula=input("Insira seu número de matrícula")
+curso=("Insira o curso em que você está matriculado")
+
+print("Nome: "+nome)
+print("Matrícula: "+matricula)
+print("Curso: "+curso)
+
