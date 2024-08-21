e0c8e9857bec4472632c0947539f215c4824776e
borgesicaro <icaroribeiro2004@gmail.com>
2024-08-20 22:58:08 -0300

    aula dois atividade 1

--- aula2_atividade1.py
+++ aula2_atividade1.py
@@ -1,5 +1,48 @@
+#atividade1
+nome1='Ícaro Ribeiro Borges '
+print(nome1)
+#atividade 2
+a=10
+b=5
+soma=a+b
+print(soma)
+#atividade 3
+numero=7
+resultado1=numero*3
+print(resultado1)
+#atividade 4
+valor1=20
+valor2=4
+resultado2=valor2-valor1
+print(resultado2)
+#atividade 5
+numerador=50
+denominador=2
+resultado3=numerador/denominador
+print(resultado3)
+#atividade 6 
+def cumprimento(nome2):
+    print('olá'+nome2)
+    cumprimento('Romes o brabo')
+    #atividade 7
+    def soma(a, b):
+    print('A soma de ambos é '+str(a+b))
+
+    soma(25, 30)
+    #atividade 8
 def multiplicar(n1, n2):
     print('número 1'+ n1)
     print('número 2'+ n2)
-    print('A multiplicação de ambos é '+n1*n2)
+    print('A multiplicação de ambos é '+(n1*n2))
+
     multiplicar(6, 3)
+    #atividade 9
+primeiro_nome='Ícaro'
+ultimo_nome='Fonseca'
+nome_completo=primeiro_nome+'_'+ultimo_nome
+#atividade 10
+def mensagem(nome3):
+    print('Bom dia'+nome3)
+
+    saudacao=mensagem('Romes o brabo da lacoste')
+    print(saudacao)
